8a508d8cb6a03aeadb8229a0b776ec057ff07cb3
gabrieledelfrate <148788158+gabrieledelfrate@users.noreply.github.com>
2024-01-30 17:49:03 +0100

    Add files via upload

---
+++ Esercitazione2C#/Persona.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Persona
+{
+	public Persona()
+	{
+    public string Nome { get; set; }
+    public string Cognome { get; set; }
+    public int Eta { get; set; }
+
+    public Persona()
+    {
+        Nome = "Non definito";
+        Cognome = "Non definito";
+        Eta = 0;
+    }
+    public Persona(string nome, string cognome, int eta)
+    {
+        Nome = nome;
+        Cognome = cognome;
+        Eta = eta;
+    }
+
+    public string GetInformazioniPersona()
+    {
+        return $"Nome: {Nome}, Cognome: {Cognome}, Età: {Eta} anni";
+    }
+}
+}
---
+++ Esercitazione2C#/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Esercitazione2C_;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Persona persona1 = new Persona(); 
+        Console.WriteLine("Informazioni sulla persona 1:");
+        Console.WriteLine(persona1.GetInformazioniPersona());
+
+        Persona persona2 = new Persona("Mario", "Rossi", 30);
+        Console.WriteLine("\nInformazioni sulla persona 2:");
+        Console.WriteLine(persona2.GetInformazioniPersona());
+    }
+}
